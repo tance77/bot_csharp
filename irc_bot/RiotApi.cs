41e4f9f4d73fa50c9d63267cc12123fb97eb9c57
Lance <starrazor31@gmail.com>
2015-11-12 09:26:46 -0800

    File Folder  Restructure

---
+++ irc_bot/RiotApi.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace twitch_irc_bot
+{
+    internal class RiotApi : WebFunctions
+    {
+        private readonly string _apiKey;
+        private readonly DatabaseFunctions _db = new DatabaseFunctions();
+
+        public RiotApi(DatabaseFunctions db)
+        {
+            _apiKey = "59b21249-afb2-4484-ad4f-842536d31437";
+            _db = db;
+        }
+
+        public Dictionary<string, int> GetMasteries(string fromChannel)
+        {
+            string summonerId = _db.GetSummonerId(fromChannel);
+            var masteriesList = new Dictionary<string, int>();
+
+            string url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/" + summonerId + "/masteries?api_key=" +
+                _apiKey;
+            string jsonString = RequestJson(url);
+            if (jsonString == "400" || jsonString == "401" || jsonString == "404" || jsonString == "429" ||
+                    jsonString == "500" || jsonString == "503")
+            {
+                return null;
+            }
+            JToken jsonArr = JObject.Parse(jsonString).SelectToken(summonerId).SelectToken("pages");
+
+            foreach (JToken page in jsonArr)
+            {
+                string current = JObject.Parse(page.ToString()).SelectToken("current").ToString();
+                if (current == "True")
+                {
+                    JToken masteriesArr = JObject.Parse(page.ToString()).SelectToken("masteries");
+                    foreach (JToken mastery in masteriesArr)
+                    {
+                        string cur = JObject.Parse(mastery.ToString()).SelectToken("id").ToString();
+                        string rank = JObject.Parse(mastery.ToString()).SelectToken("rank").ToString();
+                        int tmp;
+                        if (int.TryParse(rank, out tmp))
+                            masteriesList.Add(cur, tmp);
+                    }
+                }
+            }
+
+            var masterDictionary = new Dictionary<string, int> {{"Offense", 0}, {"Defense", 0}, {"Utility", 0}};
+
+            foreach (var masteryId in masteriesList)
+            {
+                string url2 = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/mastery/" + masteryId.Key +
+                    "?masteryData=masteryTree&api_key=" + _apiKey;
+                string masteryJson = RequestJson(url2);
+                string masteryTree = JObject.Parse(masteryJson).SelectToken("masteryTree").ToString();
+                masterDictionary[masteryTree] += masteryId.Value;
+            }
+            return masterDictionary;
+        }
+
+        public string GetSummonerId(string summonerName)
+        {
+            summonerName = summonerName.Trim(' ');
+            string url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" +
+                _apiKey;
+            string jsonString = RequestJson(url);
+            if (jsonString == "400" || jsonString == "401" || jsonString == "404" || jsonString == "429" ||
+                    jsonString == "500" || jsonString == "503")
+            {
+                return jsonString;
+            }
+            summonerName = summonerName.Replace(" ", "").ToLower();
+            //gets rids of spaces for json string riot removes spaces
+            return (JObject.Parse(jsonString).SelectToken(summonerName).SelectToken("id")).ToString();
+        }
+
+        public Dictionary<string, int> GetRunes(string fromChannel)
+        {
+            string summonerId = _db.GetSummonerId(fromChannel);
+            var runeDictionary = new Dictionary<string, int>();
+            if (summonerId == null) return null;
+            string url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/" + summonerId + "/runes?api_key=" + _apiKey;
+            string jsonString = RequestJson(url);
+            if (jsonString == "400" || jsonString == "401" || jsonString == "404" || jsonString == "429" ||
+                    jsonString == "500" || jsonString == "503")
+            {
+                return null;
+            }
+            JToken jsonArr = JObject.Parse(jsonString).SelectToken(summonerId).SelectToken("pages");
+            foreach (JToken item in jsonArr)
+            {
+                string current = JObject.Parse(item.ToString()).SelectToken("current").ToString();
+                if (current == "True")
+                {
+                    JToken runeArr = JObject.Parse(item.ToString()).SelectToken("slots");
+                    foreach (JToken rune in runeArr)
+                    {
+                        string cur = JObject.Parse(rune.ToString()).SelectToken("runeId").ToString();
+                        if (runeDictionary.ContainsKey(cur))
+                        {
+                            runeDictionary[cur] += 1;
+                        }
+                        else
+                        {
+                            runeDictionary.Add(cur, 1);
+                        }
+                    }
+                }
+            }
+            //getting the name of each rune
+            var runeDictionaryNames = new Dictionary<string, int>();
+            foreach (var id in runeDictionary)
+            {
+                string url2 = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/rune/" + id.Key + "?api_key=" +
+                    _apiKey;
+                string jsonString2 = RequestJson(url2);
+                string runeName = JObject.Parse(jsonString2).SelectToken("name").ToString();
+                runeDictionaryNames.Add(runeName, id.Value);
+            }
+            return runeDictionaryNames;
+        }
+
+        public string GetRank(string summonerId)
+        {
+            string url = "https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + summonerId + "/entry?api_key=" +
+                _apiKey;
+            string jsonString = RequestJson(url);
+            if (jsonString == "400" || jsonString == "401" || jsonString == "404" || jsonString == "429" ||
+                    jsonString == "500" || jsonString == "503")
+            {
+                return jsonString;
+            }
+            string division =
+                JObject.Parse(jsonString)
+                .SelectToken(summonerId)
+                .First.SelectToken("entries")
+                .First.SelectToken("division")
+                .ToString();
+            string lp =
+                JObject.Parse(jsonString)
+                .SelectToken(summonerId)
+                .First.SelectToken("entries")
+                .First.SelectToken("leaguePoints")
+                .ToString();
+            string tier =
+                JObject.Parse(jsonString).SelectToken(summonerId).First.SelectToken("tier").ToString().ToLower();
+            return tier + " " + division + " with " + lp + " league points";
+        }
+    }
+}
---
+++ irc_bot/Twitch/TwitchApi.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace twitch_irc_bot
+{
+    internal class TwitchApi : WebFunctions
+    {
+        public string GetStreamUptime(string fromChannel)
+        {
+            string url = "https://api.twitch.tv/kraken/streams/" + fromChannel;
+            string jsonString = RequestJson(url);
+            if (jsonString == "" || jsonString == "502" || jsonString == "404" || jsonString == "503" || jsonString == "422" || jsonString == "500" || jsonString=="504") return "Stream is offline.";
+            if (!JObject.Parse(jsonString).SelectToken("stream").HasValues)
+            {
+                return "Stream is offline.";
+            }
+            string createdAt = JObject.Parse(jsonString).SelectToken("stream").SelectToken("created_at").ToString();
+            DateTime startedAt;
+            if (!DateTime.TryParse(createdAt, out startedAt))
+            {
+                return "Could not reach Twitch API";
+            }
+            DateTime nowTime = DateTime.Now;
+            nowTime = nowTime.ToUniversalTime();
+            int onlineForHours = (nowTime - startedAt).Hours;
+            int onlineForMinutes = (nowTime - startedAt).Minutes;
+            int onlineForSeconds = (nowTime - startedAt).Seconds;
+
+            return fromChannel + " has been online for " + onlineForHours + " hours " + onlineForMinutes + " minutes " +
+                onlineForSeconds + " seconds";
+        }
+
+        //Returns weather the stream is online or not
+        public bool StreamStatus(string fromChannel)
+        {
+            string url = "https://api.twitch.tv/kraken/streams/" + fromChannel;
+            string jsonString = RequestJson(url);
+
+            if (jsonString == "" || jsonString=="503" || jsonString =="502" || !JObject.Parse(jsonString).SelectToken("stream").HasValues)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Dictionary<string, DateTime> GetRecentFollowers(string fromChannel)
+        {
+            var r = new Random();
+            int limit = r.Next(25, 100);
+            string url = "https://api.twitch.tv/kraken/channels/" + fromChannel + "/follows?limit=" + limit;
+            string jsonString = RequestJson(url);
+            var followsDictionary = new Dictionary<string, DateTime>();
+            if (jsonString == "" || jsonString == "502" || jsonString == "404" || jsonString == "503" || jsonString =="422" ||
+                    jsonString == "500" || jsonString == "504") return null;
+            if (!JObject.Parse(jsonString).HasValues || (!JObject.Parse(jsonString).SelectToken("follows").HasValues))
+            {
+                return null;
+            }
+            JToken jsonArr = JObject.Parse(jsonString).SelectToken("follows");
+            foreach (JToken item in jsonArr)
+            {
+                string createdAt = JObject.Parse(item.ToString()).SelectToken("created_at").ToString();
+                DateTime followDate;
+                if (!DateTime.TryParse(createdAt, out followDate))
+                {
+                    return null;
+                }
+                string displayName =
+                    JObject.Parse(item.ToString()).SelectToken("user").SelectToken("display_name").ToString();
+                followsDictionary.Add(displayName, followDate);
+            }
+            return followsDictionary;
+        }
+
+
+        public List<string> GetActiveUsers(string fromChannel) //via chatters json deprecated
+        {
+            var userList = new List<string>();
+            string url = "http://tmi.twitch.tv/group/user/" + fromChannel + "/chatters";
+            string jsonString = RequestJson(url);
+            if (jsonString == "" || jsonString == "502") return null;
+
+            //Line  87 is equivalent to line 89
+            //JToken modsaksjdlsakd = JObject.Parse(jsonString)["chatters"]["moderators"];
+
+            JToken mods = JObject.Parse(jsonString).SelectToken("chatters").SelectToken("moderators");
+            JToken staff = JObject.Parse(jsonString).SelectToken("chatters").SelectToken("staff");
+            JToken admins = JObject.Parse(jsonString).SelectToken("chatters").SelectToken("admins");
+            JToken globalMods = JObject.Parse(jsonString).SelectToken("chatters").SelectToken("global_mods");
+            JToken viewers = JObject.Parse(jsonString).SelectToken("chatters").SelectToken("viewers");
+            userList.AddRange(mods.Select(mod => (string) mod));
+            userList.AddRange(staff.Select(user => (string) user));
+            userList.AddRange(admins.Select(admin => (string) admin));
+            userList.AddRange(globalMods.Select(globalMod => (string) globalMod));
+            userList.AddRange(viewers.Select(viewer => (string) viewer));
+            return userList;
+        }
+        public bool CheckAccountCreation(string user)
+        {
+            string url = "https://api.twitch.tv/kraken/users/" + user;
+            string jsonString = RequestJson(url);
+            //know way of telling if the account was created today so just return false
+            if (jsonString == "" || jsonString == "502" || jsonString =="404") return false;
+            var creation_date = JObject.Parse(jsonString).SelectToken("created_at").ToString();
+            //Was the account created before today
+            var a = DateTime.Compare(DateTime.Parse(creation_date).Date.AddDays(-1) , DateTime.UtcNow.Date); //.Date just compares the date
+            //returns true if creation is > = 0 else false
+            //you're to young for links
+            return a >= 0;
+            //You're an adult please continue
+        }
+    }
+}
