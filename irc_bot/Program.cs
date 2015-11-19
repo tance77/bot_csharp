e7bfad2d6e86409598fd5c5ecf29edefe91a70eb
Lance <sirlancealot77@gmail.com>
2015-11-19 11:07:27 -0800

    Reformated Files

--- irc_bot/Program.cs
+++ irc_bot/Program.cs
@@ -6,22 +6,22 @@ using System.Threading;
 
 namespace twitch_irc_bot
 {
-	internal class Program
-	{
-		private static void Main(string[] args)
-		{
-			var ircServer = new IrcClient("irc.twitch.tv", 443, "chinnbot", "oauth:88bwsy5w33ue5ogyj5g90m8qkpmvle",false);
-			var whisperServer = new IrcClient("192.16.64.212", 443, "chinnbot", "oauth:88bwsy5w33ue5ogyj5g90m8qkpmvle", true);
-			ircServer.JoinChannel("blackmarmalade");
-			//            ircServer.JoinChannelStartup();
-			var BlockingMessageQueue = new BlockingCollection<string>();
-			var BlockingWhisperQueue = new BlockingCollection<string>();
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var ircServer = new IrcClient("irc.twitch.tv", 443, "chinnbot", "oauth:88bwsy5w33ue5ogyj5g90m8qkpmvle",false);
+            var whisperServer = new IrcClient("192.16.64.212", 443, "chinnbot", "oauth:88bwsy5w33ue5ogyj5g90m8qkpmvle", true);
+            ircServer.JoinChannel("blackmarmalade");
+            //            ircServer.JoinChannelStartup();
+            var BlockingMessageQueue = new BlockingCollection<string>();
+            var BlockingWhisperQueue = new BlockingCollection<string>();
 
-			var whisperThread = new Thread (() => whisperServer.ReadMessage (ref BlockingMessageQueue, ref BlockingWhisperQueue));
-			whisperThread.Start ();
+            var whisperThread = new Thread (() => whisperServer.ReadMessage (ref BlockingMessageQueue, ref BlockingWhisperQueue));
+            whisperThread.Start ();
 
-			ircServer.ReadMessage (ref BlockingMessageQueue, ref BlockingWhisperQueue);
+            ircServer.ReadMessage (ref BlockingMessageQueue, ref BlockingWhisperQueue);
 
-		}
-	}
+        }
+    }
 }
