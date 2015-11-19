6f5b3372764644228e61265f045baebaa196467b
Lance Thompson <starrazor31@gmail.com>
2015-11-19 12:36:44 -0800

    Revert "Removed some comments in main"
    This reverts commit 581e2cffa74ccc5fd76f2bb43b95c5751ecf8a18.

--- irc_bot/Program.cs
+++ irc_bot/Program.cs
@@ -30,6 +30,48 @@ namespace twitch_irc_bot
                 var commandHandler = new IrcCommandHandler(twitchMessage, ircServer, whisperServer);
                 commandHandler.Run();
 
+
+
+
+                // if (command != null && command == "PRIVMSG")
+                // {
+                // var handledMessage = new TwitchMessageHandler(twitchMessage, irc, whisper_server);
+                // if (CheckSpam()) continue;
+                // handledMessage.CheckCommands();
+                // }
+                // else if (command != null && command == "CLEARCHAT")
+                // {
+                //     var path = "C:\\Users\\Lance\\Documents\\GitHub\\bot_csharp\\fembot\\irc_bot\\Bad Phrases\\Bad Phrases.txt";
+                //     var goodPath = "C:\\Users\\Lance\\Documents\\GitHub\\bot_csharp\\fembot\\irc_bot\\Bad Phrases\\Good Phrases.txt";
+                //
+                //     var a = irc.ChannelHistory;
+                //     foreach(var person in a){
+                //         Console.Write(twitchMessage.Msg.Split(':')[1] + "\r\n");
+                //         if (person.GetSender() == twitchMessage.Msg.Split(':')[1])
+                //         {
+                //             string toBeWritten = ":" + person.GetSender();
+                //             foreach (var dict in person.GetMessagesWithTimes())
+                //             {
+                //                 using (StreamWriter sw = File.AppendText(path))
+                //                 {
+                //                     sw.WriteLine(toBeWritten + " :" + dict.Value + " :" + dict.Key);
+                //                 }
+                //             }
+                //         }
+                //         else
+                //         {
+                //             string toBeWritten = ":" + person.GetSender();
+                //             foreach (var dict in person.GetMessagesWithTimes())
+                //             {
+                //                 using (StreamWriter sw = File.AppendText(goodPath))
+                //                 {
+                //                     sw.WriteLine(toBeWritten + " :" + dict.Value + " :" + dict.Key);
+                //                 }
+                //             }
+                //         }
+                //     }
+                //     irc.ChannelHistory.Clear();
+                // }
             }
         }
     }
